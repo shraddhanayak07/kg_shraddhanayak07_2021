20dcff49c587f8b2ec9ecbbb2084042f25d889b5
Shraddha Nayak <shraddhanayak07@gmail.com>
2020-03-14 21:19:17 -0700

    Two changes in main.py
    1. Added try, except to handle Index Error when a user doesn't add inputs while executing the file 
    2. Add a base case: when the length of both strings is not equal, it will return False since one to one mapping will not be possible when lengths are different.

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import sys
 
 def checkMapping(s1, s2):
 	print("Received Command Line Arguments, s1:  "+str(s1)+", s2: "+str(s2))
+	if len(s1) != len(s2):
+                return False    #One to One mapping is not possible when length are different.
 
-print(checkMapping(sys.argv[1],sys.argv[2]))
+try:
+        print(checkMapping(sys.argv[1],sys.argv[2]))
+except IndexError:
+        print("Add Two Inputs")
